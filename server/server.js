555093ff5b4f6a0a928634489178e55aca87c8f8
Priyansu GoWala <priyanshugowala808@gmail.com>
2025-08-12 10:15:45 +0530

    Update CORS origin in Socket.IO initialization to allow all origins

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ export const userSocketMap = {};
 // 👉 Initialize Socket.IO
 export const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-aljs.vercel.app", // Change to frontend origin in production
+    origin: "*", // Change to frontend origin in production
     credentials: true,
   },
 });
